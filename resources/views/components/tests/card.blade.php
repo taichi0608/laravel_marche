f36433cb31adc286036d26fe61f9ca9b8ffe3382
taichi0608 <taichi.okuno0608@gmail.com>
2022-12-31 13:17:40 +0900

    props

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,3 +1,9 @@
+@props([
+    'title',
+    'message' => '初期値です',
+    'content' => '本文は初期値です',
+    ])
+
 <div class="boder-2 shadow-md w-1/4 p-2">
     <div class="">{{ $title }}</div>
     <div class="">画像</div>
