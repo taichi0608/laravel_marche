d17f2d89780265d5d7d13a9bf6eb9299aeb192c7
taichi0608 <taichi.okuno0608@gmail.com>
2022-12-31 13:46:34 +0900

    属性バッグ

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -4,7 +4,7 @@
     'content' => '本文は初期値です',
     ])
 
-<div class="boder-2 shadow-md w-1/4 p-2">
+<div {{ $attributes ->merge(['class'=> 'boder-2 shadow-md w-1/4 p-2']) }} >
     <div class="">{{ $title }}</div>
     <div class="">画像</div>
     <div class="">{{ $content }}</div>
