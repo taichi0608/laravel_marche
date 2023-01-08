2fb90c48aa8222238574039212a3845dfdb3a418
taichi0608 <taichi.okuno0608@gmail.com>
2023-01-08 17:53:20 +0900

    shop edit,update

--- database/migrations/2023_01_04_175811_create_shops_table.php
+++ database/migrations/2023_01_04_175811_create_shops_table.php
@@ -20,7 +20,7 @@ class CreateShopsTable extends Migration
             ->onUpdate('cascade')
             ->onDelete('cascade');
             $table->string('name');
-            $table->text('infomation');
+            $table->text('information');
             $table->string('filename');
             $table->boolean('is_selling');
             $table->timestamps();
